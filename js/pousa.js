90dbaa7b43b0d6a1acafcd02848f0a19c6a2f136
fyonathan <zionicxz@gmail.com>
2018-03-05 00:31:28 +0700

    Version 4

--- js/pousa.js
+++ js/pousa.js
@@ -10,18 +10,29 @@ $('body').on('keyup', '[name=harga_barang]', function(e) {
 function isNumber(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode == 44) {
+    	return true;
+    }
     if (charCode > 31 && (charCode < 48 || charCode > 57)) {
         return false;
     }
     return true;
 }
 function calc() {
-	var dr = parseInt($('[name=harga_barang]').val().split('.').join(""));
+	var number = $('[name=harga_barang]').val().split('.').join("");
+	var comma = number.substr(number.length - 1, 1) == ',';
+	var dec = number.split(',');
+	if (dec.length > 1) {
+		var dr = parseFloat(dec[0]+"."+dec[1]);
+	} else {
+		var dr = parseFloat(dec[0]);
+	}
 	if (isNaN(dr)) {
 		$('#ed').html('0');
-		$('[name=harga_barang]').val('');
+		$('[name=harga_barang]').val(comma ? '0,' : '');
 		return;
 	}
+	console.log(dr);
 	var rp = dr * 14200;
 	if (rp > 1000000) {
 		var tx = rp * 15 /100;
@@ -35,10 +46,19 @@ function calc() {
 	}
 	var ed = rp + tx + pr;
 	if (dr > 0) {
-		$('#ed').html(ed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."));
-		$('[name=harga_barang]').val(dr.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."));
+		var edS = ed.toString().split('.');
+		if (edS.length > 1) {
+			$('#ed').html(edS[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")+","+edS[1].substr(0, 2));
+		} else {
+			$('#ed').html(ed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."));
+		}
+		if (dec.length > 1) {
+			$('[name=harga_barang]').val(dec[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")+","+dec[1]);
+		} else {
+			$('[name=harga_barang]').val(dr.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + (comma ? ',' : ''));
+		}
 	} else {
 		$('#ed').html('0');
-		$('[name=harga_barang]').val('');
+		$('[name=harga_barang]').val(comma ? '0,' : '0');
 	}
 }
